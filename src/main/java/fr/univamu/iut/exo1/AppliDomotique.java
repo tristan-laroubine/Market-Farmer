16cdb274572a84e0c14c4a37ed2fc0c22fd4fdc0
Petru Valicov <petru.valicov@univ-amu.fr>
2017-10-15 20:42:20 +0200

    Saisie utilisateur au clavier

--- src/main/java/fr/univamu/iut/exo1/AppliDomotique.java
+++ src/main/java/fr/univamu/iut/exo1/AppliDomotique.java
@@ -1,7 +1,41 @@
 package fr.univamu.iut.exo1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class AppliDomotique {
+    static ArrayList<Connectable> objets = new ArrayList<Connectable>();
+
+    public static int menu() {
+        int choix = 1;
+        System.out.println("Taper 0 pour finir, 1 pour connecter une cafetière et 2 pour une radio");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            choix = Integer.parseInt(in.readLine());
+        } catch (IOException e) {
+            System.out.println("Problème de saisie");
+        }
+        return choix;
+    }
+
+    public static void connecter(int type) {
+        switch (type) {
+            case 1:
+                /* A completer */
+                break;
+            case 2:
+                /* A completer */
+                break;
+        }
+    }
+
     public static void main(String[] args) {
-        throw new RuntimeException("Not yet implemented !");
+        int type = 0;
+        while ((type = menu()) != 0) {
+            connecter(type);
+        }
+        System.out.println(objets);
     }
 }
