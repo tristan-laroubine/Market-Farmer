1f8dd3c4fda8b006925af91c1e9c7a2c0e54b16b
NAM Gregory <n17002251@a-maq-iutinfo.salsa.univ-amu.fr>
2018-12-05 09:17:52 +0100

    implementation de methode dans marche fermier

--- src/main/java/fr/univamu/iut/Marché/MarcheFermier.java
+++ src/main/java/fr/univamu/iut/Marché/MarcheFermier.java
@@ -2,20 +2,20 @@ package fr.univamu.iut.Marché;
 
 import fr.univamu.iut.Controleur;
 import fr.univamu.iut.Fermier.Producteur;
+import fr.univamu.iut.Producteur.Producteur;
 import fr.univamu.iut.ProduitFermier.ProduitFermier;
+import sun.reflect.generics.tree.Tree;
 
 import java.util.ArrayList;
+import java.util.TreeSet;
 
 public class MarcheFermier extends Marche {
 
     private Controleur c;
 
-    private ArrayList <Producteur> vendeurs;
-    private ArrayList <Acheteur>  acheteurs;
-
-    //UNE MAP POUR LES DEUX ????
-    private ArrayList<ProduitFermier> produitsFermier;
-    private ArrayList<Offre> offres;
+    private TreeSet<Producteur> vendeurs;
+    private TreeSet <Acheteur>  acheteurs;
+    private TreeSet <ProduitFermier> produitsFermier;
 
 
 
@@ -26,6 +26,58 @@ public class MarcheFermier extends Marche {
         this.c = c;
     }
 
+    public TreeSet<Producteur> getVendeurs() {
+        return vendeurs;
+    }
+
+    public TreeSet<Acheteur> getAcheteurs() {
+        return acheteurs;
+    }
+
+    public TreeSet<ProduitFermier> getProduitsFermier() {
+        return produitsFermier;
+    }
+
+    public void setVendeurs(TreeSet<Producteur> vendeurs) {
+        this.vendeurs = vendeurs;
+    }
+
+    public void setAcheteurs(TreeSet<Acheteur> acheteurs) {
+        this.acheteurs = acheteurs;
+    }
+
+    public void setProduitsFermier(TreeSet<ProduitFermier> produitsFermier) {
+        this.produitsFermier = produitsFermier;
+    }
 
 
+    public void addVendeur(Producteur p)
+    {
+        vendeurs.add(p);
+    }
+
+    public void addVendeurs(TreeSet<Producteur> vendeurs)
+    {
+        vendeurs.addAll(vendeurs);
+    }
+
+    public void addAcheteur(Acheteur a)
+    {
+        acheteurs.add(a);
+    }
+
+    public void addAcheteurs(TreeSet<Acheteur> acheteurs)
+    {
+        acheteurs.addAll(acheteurs);
+    }
+
+    public void addProduit(ProduitFermier pf)
+    {
+        produitsFermier.add(pf);
+    }
+
+    public void addProduits(TreeSet<ProduitFermier> pFermiers)
+    {
+        produitsFermier.addAll(pFermiers);
+    }
 }
