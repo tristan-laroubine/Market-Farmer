3a3b863d131f3b0a19e7abc150388228d5312ea0
MAISSE Xavier <m17001857@a-maq-iutinfo.salsa.univ-amu.fr>
2018-12-05 09:22:07 +0100

    Class Arboriculteur

--- src/main/java/fr/univamu/iut/Producteur/Arboriculteur.java
+++ src/main/java/fr/univamu/iut/Producteur/Arboriculteur.java
@@ -1,4 +1,26 @@
 package fr.univamu.iut.Producteur;
 
+import fr.univamu.iut.ProduitFermier.ProduitFermier;
+import fr.univamu.iut.UniteDeProduction.UniteDeProductionDeViande;
+
 public class Arboriculteur {
+    private UniteDeProductionArboriculteur uniteDeProductionArboriculteur;
+
+    public UUniteDeProductionArboriculteur getUniteDeProductionArboriculteur() {
+        return UniteDeProductionArboriculteur;
+    }
+
+    public void setUniteDeProductionArboriculteur(UniteDeProductionArboriculteur uniteDeProductionArboriculteur) {
+        this.uniteDeProductionArboriculteur = uniteDeProductionArboriculteur;
+    }
+
+    @Override
+    public ProduitFermier produitProduit(String type) {
+        return uniteDeProductionArboriculteur.produitProduit(type);
+    }
+
+    @Override
+    public ProduitFermier produitProduit(String type, String[] arg, Object... arguments) {
+        return uniteDeProductionArboriculteur.produitProduit(type,arg,arguments);
+    }
 }
