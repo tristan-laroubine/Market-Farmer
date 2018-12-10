acf0ddc6fd4e85febed07f5ecb9518f8eba4114a
AMGHAR Axel <a17004403@a-maq-iutinfo.salsa.univ-amu.fr>
2018-12-10 16:20:47 +0100

    getPrix dans offreachat

--- src/main/java/fr/univamu/iut/traitement/Acteur/CentraleAchat.java
+++ src/main/java/fr/univamu/iut/traitement/Acteur/CentraleAchat.java
@@ -1,18 +1,28 @@
 package fr.univamu.iut.traitement.Acteur;
 
+import fr.univamu.iut.traitement.Marché.OffreAchat;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class CentraleAchat {
 
     private ArrayList <Proprietaire> acheteurs;
+    private ArrayList<OffreAchat> offreAchats = new ArrayList<OffreAchat>;
+    private Iterator<OffreAchat> it = offreAchats.iterator();
+
     private int promotion;
     private int quantitee;
 
 
+    public void choixProduits(ArrayList<OffreAchat> offreAchats){
 
+        double prixtotal = 0;
+        while (it.hasNext()){
+            OffreAchat offreAchat = it.next();
+            prixtotal += offreAchat.get
 
-
-    public void choixProduits(){
+        }
 
     }
 
