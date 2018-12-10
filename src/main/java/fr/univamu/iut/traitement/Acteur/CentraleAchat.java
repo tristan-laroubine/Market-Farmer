0ce3c175652d14cc53bd57f4d4b6203fe6c7388a
AMGHAR Axel <a17004403@a-maq-iutinfo.salsa.univ-amu.fr>
2018-12-10 17:26:37 +0100

    choixProduits

--- src/main/java/fr/univamu/iut/traitement/Acteur/CentraleAchat.java
+++ src/main/java/fr/univamu/iut/traitement/Acteur/CentraleAchat.java
@@ -10,21 +10,39 @@ public class CentraleAchat {
     private ArrayList <Proprietaire> acheteurs;
     private ArrayList<OffreAchat> offreAchats = new ArrayList<OffreAchat>();
     private Iterator<OffreAchat> it = offreAchats.iterator();
+    private ArrayList <OffreAchat> offresChoisies = null;
+    private Iterator it2  = offresChoisies.iterator();
+    private Iterator it3 = offreAchats.iterator();
 
     private int promotion;
     private int quantitee;
 
+        public void choixProduits(ArrayList<OffreAchat> offreAchats){
 
-    public void choixProduits(ArrayList<OffreAchat> offreAchats){
-//
-//        double prixtotal = 0;
-//        while (it.hasNext()){
-//            OffreAchat offreAchat = it.next();
-//            prixtotal += offreAchat.get
+            double prixtotal = 0;
 
-//        }
+            while (it.hasNext()){
+                OffreAchat offreAchat = (OffreAchat) it.next();
+                //prixtotal ajoute chaque pris de tous les produits de la liste
+                prixtotal += offreAchat.getPrix();
+            }
 
-    }
+            //renvoie la moyenne des prix de tous les produits.
+            prixtotal /= offreAchats.size();
+
+            while (it3.hasNext()){
+                //choisi les produits
+                OffreAchat offreAchat = (OffreAchat) it3.next();
+
+                if(offreAchat.getPrix() <= prixtotal ) {
+                    OffreAchat offreAchat1 = (OffreAchat) it2.next();
+                    offresChoisies.add(offreAchat);
+                }
+            }
+
+        }
+
+
 
     public void rechercheFournisseurs(){
 
