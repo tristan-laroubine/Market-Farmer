c07e419e7242aa0d39d1fc283b9af23dc69f30a0
MAISSE Xavier <m17001857@a-maq-iutinfo.salsa.univ-amu.fr>
2018-12-10 16:28:13 +0100

    Fonction proposerOffreEnGrandeQuantitee

--- src/main/java/fr/univamu/iut/traitement/Acteur/Grossiste.java
+++ src/main/java/fr/univamu/iut/traitement/Acteur/Grossiste.java
@@ -1,16 +1,32 @@
 package fr.univamu.iut.traitement.Acteur;
 
-public class Grossiste  {
+import fr.univamu.iut.traitement.Marché.MarcheFermier;
+import fr.univamu.iut.traitement.ProduitFermier.ProduitFermier;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Grossiste extends Proprietaire {
 
 
-
-
-    public void acheterGrandeQuantitee(String type, Double prixMin, Double prixMax){
-
-    }
-
-    public void revendre(){
-
+    public void proposerOffreEnGrandeQuantitee(MarcheFermier marcheFermier, String type, Double prixMax, Double soldeMax) {
+        Comparator<ProduitFermier> produitFermierComparator = (s1, s2) -> (int) (s1.getPrix() - s2.getPrix());
+        PriorityQueue<ProduitFermier> produitFermierPriorityQueue = new PriorityQueue<>(produitFermierComparator);
+        if (soldeMax > getSolde()) soldeMax = getSolde();
+        for (ProduitFermier produitfermier : marcheFermier.getProduitsFermier()) {
+            try {
+                if(produitfermier.getClass() == Class.forName(type) && produitfermier.getPrix()<=prixMax){
+                    produitFermierPriorityQueue.add(produitfermier);
+                }
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        while(!produitFermierPriorityQueue.isEmpty() && soldeMax>=0 ){
+            ProduitFermier produitFermier = produitFermierPriorityQueue.peek();
+            soldeMax = soldeMax - produitFermier.getPrix();
+            proposerOffre(marcheFermier,produitFermier);
+        }
     }
 
 }
