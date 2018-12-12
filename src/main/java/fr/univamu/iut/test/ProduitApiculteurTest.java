0a03654bad4c1bba61cd4a52295b68bb0e7cc69b
SnK <gregory.nam@orange.fr>
2018-12-12 08:44:00 +0100

    produit apiculteur test

--- src/main/java/fr/univamu/iut/test/ProduitApiculteurTest.java
+++ src/main/java/fr/univamu/iut/test/ProduitApiculteurTest.java
@@ -1,6 +1,40 @@
 package fr.univamu.iut.test;
 
+import fr.univamu.iut.traitement.ProduitFermier.ProduitApiculteur.Miel;
+import fr.univamu.iut.traitement.ProduitFermier.ProduitApiculteur.ProduitApiculteur;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class ProduitApiculteurTest {
+
+    private ProduitApiculteur produitApiculteur;
+    private List<String> labels = new ArrayList<>();
+
+    @Test
+    public final void attributMielTest()
+    {
+        produitApiculteur= new Miel(labels,LocalDate.of(1995,10,12) , 10,25.0);
+        assertEquals(produitApiculteur.getPoids(), 10);
+        assertEquals("C'est bon", produitApiculteur.getPrix(), 25, 0);
+        assertEquals(produitApiculteur.getDatePeremption(),LocalDate.of(1995,10,12));
+    }
+
+    @Test
+    public final void validerMielNonPerime()
+    {
+        produitApiculteur = new Miel(labels,LocalDate.of(2020,12,12) , 10,25.0);
+        assertTrue(produitApiculteur.valider());
+    }
+
+    @Test
+    public final void validerMielPerime(){
+
+        produitApiculteur = new Miel(labels,LocalDate.of(2014,12,12) , 10,25.0);
+        assertFalse(produitApiculteur.valider());
+    }
 }
