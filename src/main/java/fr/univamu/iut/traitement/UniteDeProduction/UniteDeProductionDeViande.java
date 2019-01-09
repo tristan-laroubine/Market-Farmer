c3a6fbb9fb59d9ed803dd749a522fbe41805acf6
NAM Gregory <n17002251@a-maq-iutinfo.salsa.univ-amu.fr>
2019-01-09 11:44:12 +0100

    unite de production de viande doc

--- src/main/java/fr/univamu/iut/traitement/UniteDeProduction/UniteDeProductionDeViande.java
+++ src/main/java/fr/univamu/iut/traitement/UniteDeProduction/UniteDeProductionDeViande.java
@@ -3,13 +3,28 @@ package fr.univamu.iut.traitement.UniteDeProduction;
 import fr.univamu.iut.traitement.ProduitFermier.ProduitFermier;
 import fr.univamu.iut.traitement.ProduitFermier.ProduitViande.ProduitViande;
 
+/**
+ * classe unite de production de viande
+ */
 public class UniteDeProductionDeViande extends UniteDeProduction {
+
+
+    /**
+     * Permet de produire un produit fermier de viande grace à la fonction mère
+     * @param type
+     * @return ProduitFermier
+     */
 
     @Override
     public ProduitFermier produitProduit(String type) {
         return (ProduitFermier)(ProduitViande) super.produitProduit(type);
     }
 
+    /**
+     * Permet de produire un produit fermier de viande grace à la fonction mère
+     * @param type
+     * @return ProduitFermier
+     */
     @Override
     public ProduitFermier produitProduit(String type, String[] args, Object... arguments) {
 
