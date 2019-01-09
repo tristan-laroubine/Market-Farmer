45180a3f6a7d9f5877e7999618c50621a83e3ff1
NAM Gregory <n17002251@a-maq-iutinfo.salsa.univ-amu.fr>
2019-01-09 11:36:32 +0100

    unitedeproductioapiculteur doc

--- src/main/java/fr/univamu/iut/traitement/UniteDeProduction/UniteDeProductionApiculteur.java
+++ src/main/java/fr/univamu/iut/traitement/UniteDeProduction/UniteDeProductionApiculteur.java
@@ -3,12 +3,28 @@ package fr.univamu.iut.traitement.UniteDeProduction;
 import fr.univamu.iut.traitement.ProduitFermier.ProduitApiculteur.ProduitApiculteur;
 import fr.univamu.iut.traitement.ProduitFermier.ProduitFermier;
 
+/**
+ * Classe UniteDeProductionApiculteur
+ */
 public class UniteDeProductionApiculteur extends UniteDeProduction{
+
+    /**
+     * Permet de produire un produit fermier grace à la fonction mère
+     * @param type
+     * @return ProduitFermier
+     */
     @Override
     public ProduitFermier produitProduit(String type) {
         return (ProduitFermier)(ProduitApiculteur) super.produitProduit(type);
     }
 
+    /**
+     * Permet de produire un produit fermier grace à la fonction mère
+     * @param type
+     * @param args
+     * @param arguments
+     * @return ProduitFermier
+     */
     @Override
     public ProduitFermier produitProduit(String type, String[] args, Object... arguments) {
 
