b4780511bb32bf5fc524c7067d431ebd89bd2c37
NAM Gregory <n17002251@a-maq-iutinfo.salsa.univ-amu.fr>
2019-01-09 11:45:45 +0100

    unite de production laitier doc

--- src/main/java/fr/univamu/iut/traitement/UniteDeProduction/UniteDeProductionLaitier.java
+++ src/main/java/fr/univamu/iut/traitement/UniteDeProduction/UniteDeProductionLaitier.java
@@ -3,11 +3,27 @@ package fr.univamu.iut.traitement.UniteDeProduction;
 import fr.univamu.iut.traitement.ProduitFermier.ProduitFermier;
 import fr.univamu.iut.traitement.ProduitFermier.ProduitLaitier.ProduitLaitier;
 
+/**
+ * Classe UniteDeProductionLaitier
+ */
 public class UniteDeProductionLaitier extends UniteDeProduction{
+
+    /**
+     * Permet de produire un produit fermier laitier grace à la fonction mère
+     * @param type
+     * @return ProduitFermier
+     */
+
     @Override
     public ProduitFermier produitProduit(String type) {
         return (ProduitFermier)(ProduitLaitier) super.produitProduit(type);
     }
+
+    /**
+     * Permet de produire un produit fermier laitier grace à la fonction mère
+     * @param type
+     * @return ProduitFermier
+     */
 
     @Override
     public ProduitFermier produitProduit(String type, String[] args, Object... arguments) {
