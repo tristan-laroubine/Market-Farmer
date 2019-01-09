1dc28b672dd171f409b4eb6e196309d19f4a30dc
MAISSE Xavier <m17001857@a-maq-iutinfo.salsa.univ-amu.fr>
2019-01-09 11:48:58 +0100

    Doc class Vache

--- src/main/java/fr/univamu/iut/traitement/ProduitFermier/ProduitViande/Vache.java
+++ src/main/java/fr/univamu/iut/traitement/ProduitFermier/ProduitViande/Vache.java
@@ -8,15 +8,29 @@ import java.util.List;
 public class Vache extends ProduitViande {
 
 
+    /**
+     * Constructeur vache vide
+     */
     public Vache(){
     }
 
+    /**
+     * Constructeur vache avec label, poids et prix
+     * @param labels
+     * @param poids
+     * @param prix
+     */
     public Vache(List<String> labels, Integer poids, Double prix) {
         this.labels = labels;
         this.poids = poids;
         this.prix = prix;
     }
 
+    /**
+     * Constructeur vache avec poids et prix
+     * @param poids
+     * @param prix
+     */
     public Vache(Integer poids, Double prix) {
         this.poids = poids;
         this.prix = prix;
