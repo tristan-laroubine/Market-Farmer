9c2a412fd3540b9c385acfd1ddc515a4b8857d17
MAISSE Xavier <m17001857@a-maq-iutinfo.salsa.univ-amu.fr>
2019-01-09 11:33:19 +0100

    Doc class ProduitApiculteur

--- src/main/java/fr/univamu/iut/traitement/ProduitFermier/ProduitApiculteur/ProduitApiculteur.java
+++ src/main/java/fr/univamu/iut/traitement/ProduitFermier/ProduitApiculteur/ProduitApiculteur.java
@@ -3,7 +3,9 @@ package fr.univamu.iut.traitement.ProduitFermier.ProduitApiculteur;
 
 import fr.univamu.iut.traitement.ProduitFermier.ProduitPeremption;
 
-
+/**
+ * Classe ProduitAPiculteur étends la classe ProduitPeremption
+ */
 public abstract class ProduitApiculteur extends ProduitPeremption {
 
 }
