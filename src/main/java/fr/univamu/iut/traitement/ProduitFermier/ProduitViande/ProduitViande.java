6fb88387017da045c5897ef62b5afb43bd888fc4
MAISSE Xavier <m17001857@a-maq-iutinfo.salsa.univ-amu.fr>
2019-01-09 15:35:04 +0100

    Doc class ProduitViande

--- src/main/java/fr/univamu/iut/traitement/ProduitFermier/ProduitViande/ProduitViande.java
+++ src/main/java/fr/univamu/iut/traitement/ProduitFermier/ProduitViande/ProduitViande.java
@@ -2,6 +2,9 @@ package fr.univamu.iut.traitement.ProduitFermier.ProduitViande;
 
 import fr.univamu.iut.traitement.ProduitFermier.ProduitFermier;
 
+/**
+ * Classe ProduitViande étends la classe ProduitPeremption
+ */
 public abstract class ProduitViande extends ProduitFermier {
 
 
