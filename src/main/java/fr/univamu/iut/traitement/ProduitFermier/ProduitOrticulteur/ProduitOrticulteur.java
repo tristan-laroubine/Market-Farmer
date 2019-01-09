3f37fbf63bacded718813a5de795fa47e5975c63
MAISSE Xavier <m17001857@a-maq-iutinfo.salsa.univ-amu.fr>
2019-01-09 11:43:27 +0100

    Doc class ProduitOrticulteur

--- src/main/java/fr/univamu/iut/traitement/ProduitFermier/ProduitOrticulteur/ProduitOrticulteur.java
+++ src/main/java/fr/univamu/iut/traitement/ProduitFermier/ProduitOrticulteur/ProduitOrticulteur.java
@@ -2,6 +2,9 @@ package fr.univamu.iut.traitement.ProduitFermier.ProduitOrticulteur;
 
 import fr.univamu.iut.traitement.ProduitFermier.ProduitPeremption;
 
+/**
+ * Classe ProduitOrticulteur étends la classe ProduitPeremption
+ */
 public abstract class ProduitOrticulteur extends ProduitPeremption {
 
 }
