2d4333597887e5745d985e22a5990136b1cee599
MAISSE Xavier <m17001857@a-maq-iutinfo.salsa.univ-amu.fr>
2019-01-09 11:37:18 +0100

    Doc class ProduitArboriculteur

--- src/main/java/fr/univamu/iut/traitement/ProduitFermier/ProduitArboriculteur/ProduitArboriculteur.java
+++ src/main/java/fr/univamu/iut/traitement/ProduitFermier/ProduitArboriculteur/ProduitArboriculteur.java
@@ -1,6 +1,10 @@
 package fr.univamu.iut.traitement.ProduitFermier.ProduitArboriculteur;
 import fr.univamu.iut.traitement.ProduitFermier.ProduitPeremption;
 
+
+/**
+ * Classe ProduitArboriculteur étends la classe ProduitPeremption
+ */
 public abstract class ProduitArboriculteur extends ProduitPeremption {
 
 
