9947fb3dd2a2f705ab47819ebfeb4da07eb9d1fb
MAISSE Xavier <m17001857@a-maq-iutinfo.salsa.univ-amu.fr>
2019-01-09 11:40:47 +0100

    Doc class ProduitLaitier

--- src/main/java/fr/univamu/iut/traitement/ProduitFermier/ProduitLaitier/ProduitLaitier.java
+++ src/main/java/fr/univamu/iut/traitement/ProduitFermier/ProduitLaitier/ProduitLaitier.java
@@ -2,6 +2,9 @@ package fr.univamu.iut.traitement.ProduitFermier.ProduitLaitier;
 
 import fr.univamu.iut.traitement.ProduitFermier.ProduitPeremption;
 
+/**
+ * Classe ProduitLaitier étends la classe ProduitPeremption
+ */
 public abstract class ProduitLaitier  extends ProduitPeremption {
 
 }
